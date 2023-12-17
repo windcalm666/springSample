bb30f1a16098cbe4277a6c600636b5e3076dba2d
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2023-12-17 19:59:03 +0900

    テストコード追加　controller単体テスト

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -1,16 +1,18 @@
 package com.example.demo.controller;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.service.UserService;
 
 /**
@@ -18,12 +20,12 @@ import com.example.demo.service.UserService;
  */
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
-
-    @InjectMocks
-    private UserController userController;
-
+	
     @MockBean
-    private UserService userService;
+    UserService userService;
+	
+    @Autowired
+    private UserController userController;
 
     private MockMvc mockMvc;
 
@@ -46,4 +48,38 @@ public class UserControllerTest {
             .andExpect(model().attributeExists("userRequest"))
             .andExpect(view().name("user/add"));
     }
+    
+    /**
+     * テストメソッド  userCreate() 正常系テスト
+     */
+    @Test
+    public void testCreateUserSucess() throws Exception {
+    	UserRequest testRequest = new UserRequest();
+    	testRequest.setName("名前テスト");
+    	testRequest.setAddress("住所テスト");
+    	testRequest.setPhone("090-1234-5678");
+    	
+    	mockMvc.perform((post("/user/create")).flashAttr("userRequest", testRequest))
+    		.andExpect(model().hasNoErrors())
+    		.andExpect(model().attribute("userRequest", testRequest))
+    		.andExpect(view().name("redirect:/user/list"));
+    	
+    	verify(userService, times(1)).create(testRequest);
+    }
+    
+    /**
+     *  テストメソッド userCreate() 異常系テスト
+     */
+    @Test
+    public void testCreateUserError() throws Exception {
+    	UserRequest testRequest = new UserRequest();
+    	testRequest.setName("");
+    	testRequest.setAddress("");
+    	testRequest.setPhone("");
+    	
+    	mockMvc.perform((post("/user/create")).flashAttr("userRequest", testRequest))
+    		.andExpect(model().hasErrors())
+    		.andExpect(model().attribute("userRequest",testRequest))
+    		.andExpect(view().name("user/add"));
+    	}
 }
